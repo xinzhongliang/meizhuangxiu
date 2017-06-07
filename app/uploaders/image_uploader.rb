901669e582b85799dd8824558362db1ce8fe1fac
梁信众 <johnson.liangxinzhong@gmail.com>
2017-06-07 14:09:04 +0800

    reinstall image uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,18 +2,11 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-   include CarrierWave::MiniMagick
+  # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  #storage :file
+  storage :file
   # storage :fog
-
-  if Rails.env.production?
-    storage :qiniu
-  elsif Rails.env.development?
-    storage :qiniu
-  end
-
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -52,15 +45,5 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
-
-  process resize_to_fit: [800, 800]
-
-  version :thumb do
-    process resize_to_fill: [200, 200]
-  end
-
-  version :medium do
-    process resize_to_fill: [400, 400]
-  end
 
 end
