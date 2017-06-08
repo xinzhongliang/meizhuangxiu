4c904eb4d596f735aae26b9608c9d08eef64ed2d
梁信众 <johnson.liangxinzhong@gmail.com>
2017-06-08 20:25:54 +0800

    update route for rucaptcha

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :user, controllers: {
-    passwords: 'users/passwords',
-    registrations: 'users/registrations',
-    sessions: 'users/sessions'
-  }
+  devise_for :user, 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   namespace :admin do
